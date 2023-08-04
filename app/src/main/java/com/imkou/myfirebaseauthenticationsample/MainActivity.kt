5686343e6e9e35adea52d348fd29dfb190660eb6
imkou <imkou.8127@gmail.com>
2023-08-05 01:45:07 +0900

    クリック処理とEdittextの値を取得するメソッドを作成

--- app/src/main/java/com/imkou/myfirebaseauthenticationsample/MainActivity.kt
+++ app/src/main/java/com/imkou/myfirebaseauthenticationsample/MainActivity.kt
@@ -11,5 +11,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSignUp.setOnClickListener {
+
+        }
+        binding.btnLogin.setOnClickListener {
+
+        }
+        binding.btnLogout.setOnClickListener {
+
+        }
+    }
+
+    /**
+     * EditTextに入力された値を取得する
+     * @return email password
+     */
+    private fun getAuthenticationInfo(): Pair<String?, String?> {
+        // EditTextに入力されていないときはnullを返却する
+        val email = binding.etEmail.text.toString().takeIf { it.isNotEmpty() }
+        val password = binding.etPassword.text.toString().takeIf { it.isNotEmpty() }
+
+        return Pair(email,password)
     }
 }
