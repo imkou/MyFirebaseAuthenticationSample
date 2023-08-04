7bb1eab4b4ec35c53144f4f02accc786b1abc28b
imkou <imkou.8127@gmail.com>
2023-08-05 02:30:11 +0900

    ボタン押下時の処理追加

--- app/src/main/java/com/imkou/myfirebaseauthenticationsample/MainActivity.kt
+++ app/src/main/java/com/imkou/myfirebaseauthenticationsample/MainActivity.kt
@@ -36,14 +36,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // SignUpボタン押下時の処理
         binding.btnSignUp.setOnClickListener {
-
+            val (email, password) = getAuthenticationInfo()
+            if (email != null && password != null) {
+                val instance = MyFirebaseAuthenticationUtil.instance
+                // リスナーを登録
+                instance.setAuthenticationListener(authenticationListener)
+                instance.signUp(email, password)
+            } else {
+                // 認証情報が入力されていないときの処理
+            }
         }
+
+        // Loginボタン押下時の処理
         binding.btnLogin.setOnClickListener {
-
+            val (email, password) = getAuthenticationInfo()
+            if (email != null && password != null) {
+                val instance = MyFirebaseAuthenticationUtil.instance
+                // リスナーを登録
+                instance.setAuthenticationListener(authenticationListener)
+                instance.login(email, password)
+            } else {
+                // 認証情報が入力されていないときの処理
+            }
         }
-        binding.btnLogout.setOnClickListener {
 
+        // Logoutボタン押下時の処理
+        binding.btnLogout.setOnClickListener {
+            val instance = MyFirebaseAuthenticationUtil.instance
+            val loginStatus = instance.loginStatus()
+            if (loginStatus) {
+                // Login中の処理
+                instance.logout()
+                Toast.makeText(applicationContext, "Logoutしました", Toast.LENGTH_SHORT).show()
+            } else {
+                // Loginしていないときの処理
+            }
         }
     }
 
